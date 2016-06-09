373dc88180e5fb8b177ebea1b3a06e8839956a11
rafaelcalpena <rafael.calpena@gmail.com>
2016-06-09 15:03:01 -0400

    improvements for profile page

--- join-files/controllers/templateController.js
+++ join-files/controllers/templateController.js
@@ -36,6 +36,46 @@ angular.module('includeExample', [])
   $scope.isSignedIn = false;
 }])
 
+.controller('EditController', ['$scope', function($scope){
+	$scope.fields = {
+		name: {value:"Rafael Calpena Rodrigues", isEditing:false},
+		about: {value:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue aliquet velit, vel volutpat libero dignissim in. Morbi eu volutpat metus. Suspendisse arcu lectus, ullamcorper eget elit nec, semper feugiat nisl. Suspendisse tempor metus a scelerisque ullamcorper. Vestibulum feugiat nibh in mauris mollis viverra ultrices convallis nibh. Vestibulum at blandit eros, eget efficitur eros. Cras ultrices justo eros, non vulputate enim blandit vel. Aliquam aliquet arcu porta, iaculis massa ac, commodo magna. Proin ullamcorper feugiat risus, ac tincidunt nulla iaculis in. Fusce sed feugiat quam. Mauris imperdiet ornare diam, non gravida nisl. Proin sollicitudin elit et consectetur pellentesque. Praesent ligula ligula, pretium quis sodales eget, pulvinar sit amet augue. Praesent lobortis, enim non feugiat lobortis, lectus lectus molestie sapien, in cursus ex mauris a tortor. Nullam blandit, odio ac iaculis dictum, dolor sem aliquet tortor, eget aliquam tortor urna non lorem. Cras tempor eleifend nunc non eleifend. Suspendisse auctor diam a metus varius, eu consequat odio aliquam. Nulla suscipit dictum dictum. Donec tortor ligula, accumsan vitae ex quis, finibus lobortis erat. Curabitur sapien orci, ornare quis varius eu, bibendum at risus. Etiam ac eros eros. Mauris semper luctus eros. Maecenas varius ligula eget arcu ultrices, id ullamcorper sem dignissim. Nulla sed quam varius, vestibulum odio ac, condimentum lorem. Curabitur sem eros, elementum id placerat suscipit, varius vitae ipsum.Phasellus id lorem est. Morbi quis est finibus, aliquam nulla eu, facilisis lorem. Duis sit amet enim dui. Donec condimentum lectus nec nisl commodo tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor tempus eros quis fringilla. Duis eu nisl mattis, lacinia ipsum eu, consequat tellus. Aliquam et purus quis erat faucibus pulvinar nec ut odio. Suspendisse dignissim orci sed placerat vulputate. Vivamus in mauris bibendum odio aliquam placerat eget tincidunt nulla.", isEditing:false},
+		pictures: [{url:"http://placekitten.com/500/300"}],
+		contact: [{title:"Email", value:"someemailhere", isEditing:false}, {title:"Email", value:"someemailhere", isEditing:false}]
+	};
+	$scope.toggleEditMode= function(item, subitem){
+		if (!subitem) $scope.fields[item].isEditing = !$scope.fields[item].isEditing;
+		else $scope.fields[item].isEditing = !$scope.fields[item].isEditing;
+
+
+		if(item === "about" && $scope.fields[item].isEditing){
+			$("#about-text").attr('contentEditable','true')
+		} else {
+			$("#about-text").attr('contentEditable','false')
+
+		}
+		$scope.fields.about.value = $("#about-text").text();
+
+		if(item === "contact" && $scope.fields[item][subitem].isEditing){
+			$($("contactWrapper")[subitem]).attr('contentEditable','true')
+		}else {
+			$($("contactWrapper")[subitem]).attr('contentEditable','false')
+
+		}
+	//direct function to contentEditable
+    }
+
+	$scope.addContact = function(){
+		$scope.fields.contact.push({title:"Title (E-mail, phone, etc)...", value:"Value", isEditing:false});
+	}
+
+	$scope.removeContact = function(index){
+		alert(index);
+		$scope.fields.contact.splice(index,1);
+	}
+
+
+}])
 
 .directive("scroll", function ($window) {
     return function(scope, element, attrs) {
@@ -58,7 +98,7 @@ angular.module('includeExample', [])
 		//var initialOffset = $(profileTitle).offset().top + $(".pre-negative").offset().top;
 		var initialOffset = -55 + 400;
 
-		console.log('initialOffset is' , initialOffset)
+		//console.log('initialOffset is' , initialOffset)
 
 	    var bgHeight; //will be set when the image loads
 	    var bgWrapHeight = $(bgWrap).height(); //
@@ -79,7 +119,7 @@ angular.module('includeExample', [])
 	    function updateScroll() {
 	        var scrollTop = $(window).scrollTop();
 	        var d = initialOffset - scrollTop - navbarHeight;
-	        console.log(d)
+	        //console.log(d)
 
 	        var imgPercentage = calculateY({
 	            x: initialDiff,
